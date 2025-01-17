84f20e6196adf211783c49202d1eec231326d04f
Kunnaaallllll <869796kunalchaudhary@gmail.com>
2025-01-17 12:22:52 +0530

    here is the divide part

---
+++ divide.java
@@ -0,0 +1,12 @@
+package capgemini.calculator1;
+import java.util.*;
+public class divide {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the numbers :");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int sum=n1/n2;
+        System.out.println("THe sum of your given numbers is : "+sum);
+    }
+}
