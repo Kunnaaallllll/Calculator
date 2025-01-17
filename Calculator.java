c91f70ab2feef3ec54870c12f8761c8fbfc3601e
Kunnaaallllll <869796kunalchaudhary@gmail.com>
2025-01-17 11:46:52 +0530

    Here is the sum part of the calculator.

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package capgemini.calculator1;
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the numbers :");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int n3=sc.nextInt();
+        int sum=n1+n2+n3;
+        System.out.println("THe sum of your given numbers is : "+sum);
+    }
+}
