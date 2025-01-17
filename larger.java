5c47e039e08e0340f2f8fb7fecfd00fb75d9d1f3
Kunnaaallllll <869796kunalchaudhary@gmail.com>
2025-01-17 13:57:38 +0530

    last and final repo

---
+++ larger.java
@@ -0,0 +1,16 @@
+package capgemini.calculator1;
+import java.util.*;
+public class larger {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the numbers :");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int sum=0;
+        if(n1>n2){
+            sum=n1;
+        }
+        sum=n2;
+        System.out.println("THe sum of your given numbers is : "+sum);
+    }
+}
